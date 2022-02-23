e6d2bc030896a34de731b5162c8ea9fcdb61a688
Alejandra Landa <aleriquene@gmail.com>
2022-02-22 20:39:47 -0600

    commit tree 3b8dd613a456ef7b169eb3d5ebed2379c381dfbd

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+const { tsParticles } = require("tsparticles");
+
+function scuberGreetingForFeet(number){
+ if (number<= 400) {
+   return 'This one is on me!'
+ } else if (number>=2000 && number <=2499) 
+     return 'I will gladly take your thirty bucks.'
+   else (number>=2500) 
+     return 'No can do.'
+   }
+ 
   // Write your code here!
+
+function ternaryCheckCity(blah){
+  // Write your code here!
+  const city = blah;
+  const cityMemo = city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return cityMemo;
+
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tipString){
   // Write your code here!
+  switch(tipString){
+    case "generous":
+      return "Thank you so much."
+      case "not as generous":
+      return "Thank you."
+      case "thanks for everything":
+return "Bye."
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
